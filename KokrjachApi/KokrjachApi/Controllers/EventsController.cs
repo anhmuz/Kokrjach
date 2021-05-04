cb12c2d1c51681cfff9d5a0c86377f0b0d21326d
anhmuz <anhelina.muzychenko@gmail.com>
2021-05-04 17:42:14 +0300

    Add list of events. Handle query parameter

--- KokrjachApi/KokrjachApi/Controllers/EventsController.cs
+++ KokrjachApi/KokrjachApi/Controllers/EventsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using KokrjachApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 
 namespace KokrjachApi.Controllers
 {
@@ -12,8 +13,34 @@ namespace KokrjachApi.Controllers
     [Route("api/[controller]")]
     public class EventsController : ControllerBase
     {
-
         private readonly ILogger<EventsController> _logger;
+        private static List<Event> _events = new List<Event>()
+        {
+            new Event()
+            {
+                UserId = "Vadym",
+                EventTypeId = "Сash withdrawal",
+                Description = "Vadym withdrew cash"
+            },
+            new Event()
+            {
+                UserId = "Anhelina",
+                EventTypeId = "Money transfer",
+                Description = "Anhelina transferred money"
+            },
+            new Event()
+            {
+                UserId = "Wolter",
+                EventTypeId = "Сash withdrawal",
+                Description = "Wolter withdrew cash"
+            },
+            new Event()
+            {
+                UserId = "Kokrjach",
+                EventTypeId = "Money transfer",
+                Description = "Kokrjach transferred money"
+            }
+        };
 
         public EventsController(ILogger<EventsController> logger)
         {
@@ -23,39 +50,28 @@ namespace KokrjachApi.Controllers
         [HttpGet]
         public IEnumerable<Event> GetEvents()
         {
-            var result = new List<Event>();
-            result.Add(new Event()
-            {
-                UserId = "Vadym",
-                EventTypeId = "Сash withdrawal",
-                Description = "Vadym withdrew cash"
-            });
-            result.Add(new Event()
-            {
-                UserId = "Anhelina",
-                EventTypeId = "Money transfer",
-                Description = "Anhelina transferred money"
-            });
             Console.WriteLine("Get a list of events");
-            return result;
+            StringValues eventTypeId;
+            bool hasEventTypeIdParam = HttpContext.Request.Query.TryGetValue(nameof(Event.EventTypeId), out eventTypeId);
+            if (!hasEventTypeIdParam)
+            {
+                return _events;
+            }
+            return _events.Where(item => item.EventTypeId == eventTypeId.ToString());
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Event> GetEvent(long id)
+        public ActionResult<Event> GetEvent(int id)
         {
-            return new Event()
-            {
-                UserId = "Vadym",
-                EventTypeId = "Сash withdrawal",
-                Description = "Vadym withdrew cash"
-            };
+            return _events[id - 1];
         }
 
         [HttpPost]
         public ActionResult<Event> Post(Event eventItem)
         {
+            _events.Add(eventItem);
             Console.WriteLine("Post a new event: {0}", eventItem);
-            return CreatedAtAction(nameof(GetEvent), new { id = "1" }, eventItem);
+            return CreatedAtAction(nameof(GetEvent), new { id = _events.Count }, eventItem);
         }
     }
 }
