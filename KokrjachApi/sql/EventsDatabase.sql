b3ca51c2fd401abd0adc33956b972595f12e738a
anhmuz <anhelina.muzychenko@gmail.com>
2021-09-28 17:01:46 +0300

    Add kokrjach_events database

---
+++ KokrjachApi/sql/EventsDatabase.sql
@@ -0,0 +1,12 @@
+use kokrjach_events;
+
+drop table if exists event_item;
+
+create table event_item (
+	id int not null auto_increment,
+    user_id varchar(255) not null,
+    event_description varchar(255),
+    primary key (id)
+);
+
+GRANT SELECT (user_id), INSERT (user_id) ON event_item TO ''@'%';
